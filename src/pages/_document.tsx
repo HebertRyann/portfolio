1bee970d94d0d328af5ba1297afb26aabd64aab1
HebertRyann <hebertryann40@gmail.com>
2021-07-06 01:34:02 -0300

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/portfolio/assets/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Roboto+Slab:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Roboto+Slab:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
