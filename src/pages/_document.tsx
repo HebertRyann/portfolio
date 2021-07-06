b07ab8a1b2c1c22d545124b38713c661b2b69d15
HebertRyann <hebertryann40@gmail.com>
2021-07-06 01:37:22 -0300

    dd tag title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Hebert Ryan</title>
           <link rel="shortcut icon" href="/portfolio/assets/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Roboto+Slab:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
