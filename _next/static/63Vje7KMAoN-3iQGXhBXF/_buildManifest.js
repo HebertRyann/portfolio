11d1b5bf4c22c44e51db5a8cf1c748ff647df20d
HebertRyann <hebertryann40@gmail.com>
2021-08-05 14:20:37 +0000

    Deploying to gh-pages from @ HebertRyann/portfolio@6edbb29dc0092341ac565f2074c9fa3ed7b07ffc 🚀

--- _next/static/63Vje7KMAoN-3iQGXhBXF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6cbf8931496f448f2fb0.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
